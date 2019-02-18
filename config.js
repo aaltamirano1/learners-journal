941b808a01681f719ef75c1f85b915349aa832e5
Angel Altamirano <angel.g.altam@gmail.com>
2019-02-18 14:02:51 -0500

    connect to mLab:

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/learners-journal';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://aaltamirano1:crashfest2@ds119930.mlab.com:19930/learners-journal';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/learners-journal-test';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
